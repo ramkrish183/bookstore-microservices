07e34ec654fdbc271e580117c23ee65c28f6d59f
RameshJ <rameshkrishnan183@gmail.com>
2024-08-20 16:36:38 -0400

    catalog service - getproducts and getproductbyCode apis

--- catalog-service/src/main/java/com/rameshj/bookstore/catalog/domain/ProductEntity.java
+++ catalog-service/src/main/java/com/rameshj/bookstore/catalog/domain/ProductEntity.java
@@ -4,7 +4,6 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-
 import java.math.BigDecimal;
 
 @Entity
--- catalog-service/src/main/java/com/rameshj/bookstore/catalog/web/controllers/ProductController.java
+++ catalog-service/src/main/java/com/rameshj/bookstore/catalog/web/controllers/ProductController.java
@@ -34,5 +34,4 @@ public class ProductController {
                 .map(ResponseEntity::ok)
                 .orElseThrow(() -> ProductNotFoundException.forCode(code));
     }
-
 }
