668a0e6740ca1672e9423d3a24e179292ad7e331
RameshJ <rameshkrishnan183@gmail.com>
2024-08-31 14:10:59 -0400

    order-service skeleton

---
+++ order-service/src/main/java/com/rameshj/bookstore/orders/web/RabbitMQDemoController.java
@@ -0,0 +1,29 @@
+package com.rameshj.bookstore.orders.web;
+
+import com.rameshj.bookstore.orders.ApplicationProperties;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RabbitMQDemoController {
+
+    private final RabbitTemplate rabbitTemplate;
+
+    private final ApplicationProperties properties;
+
+    public RabbitMQDemoController(RabbitTemplate rabbitTemplate, ApplicationProperties properties) {
+        this.rabbitTemplate = rabbitTemplate;
+        this.properties = properties;
+    }
+
+    @PostMapping("/send")
+    public void sendMessage(@RequestBody MyMessage message){
+        rabbitTemplate.convertAndSend(properties.orderEventsExchange(),message.routingKey(),message.payload());
+    }
+
+    record MyMessage (String routingKey, PayLoad payload){}
+    record PayLoad(String content){}
+
+}
---
+++ order-service/src/main/java/com/rameshj/bookstore/orders/web/RabbitMQListener.java
@@ -0,0 +1,17 @@
+package com.rameshj.bookstore.orders.web;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RabbitMQListener {
+    @RabbitListener(queues = "${orders.new-orders-queue}")
+    public void handleNewOrder(RabbitMQDemoController.PayLoad message){
+        System.out.println("New order --> "+ message.content());
+    }
+
+    @RabbitListener(queues = "${orders.delivered-orders-queue}")
+    public void handleDeliveredOrder(RabbitMQDemoController.PayLoad message){
+        System.out.println("Delivered order --> "+ message.content());
+    }
+}
