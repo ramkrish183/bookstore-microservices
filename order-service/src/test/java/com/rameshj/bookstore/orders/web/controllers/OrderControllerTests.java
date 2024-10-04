dff029e14c56b0dfef82bf744f993a41ffb9b782
RameshJ <rameshkrishnan183@gmail.com>
2024-10-03 21:16:38 -0400

    removed imports

--- order-service/src/test/java/com/rameshj/bookstore/orders/web/controllers/OrderControllerTests.java
+++ order-service/src/test/java/com/rameshj/bookstore/orders/web/controllers/OrderControllerTests.java
@@ -1,20 +1,6 @@
 package com.rameshj.bookstore.orders.web.controllers;
 
-import static io.restassured.RestAssured.given;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.hamcrest.CoreMatchers.is;
-
 import com.rameshj.bookstore.orders.AbstractIT;
-import com.rameshj.bookstore.orders.domain.models.OrderSummary;
-import com.rameshj.bookstore.orders.testdata.TestDataFactory;
-import io.restassured.common.mapper.TypeRef;
-import io.restassured.http.ContentType;
-import java.util.List;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
-import org.springframework.http.HttpStatus;
-import org.springframework.test.context.jdbc.Sql;
 
-//@Sql("/test-orders.sql")
-class OrderControllerTests extends AbstractIT {
-}
+// @Sql("/test-orders.sql")
+class OrderControllerTests extends AbstractIT {}
